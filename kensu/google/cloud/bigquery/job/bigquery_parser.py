3bb622eb1ee549e5237109310784b0d6f6e76eb7
vidmantas zemleris <vidma@kensu.io>
2021-08-13 16:42:04 +0300

    refactor

--- kensu/google/cloud/bigquery/job/bigquery_parser.py
+++ kensu/google/cloud/bigquery/job/bigquery_parser.py
@@ -128,48 +128,53 @@ class BqRemoteParser:
         parsed_resp = lineage_resp.json()
         lineage_info = parsed_resp['lineage']
         stats_info = parsed_resp['stats']
-        inputs = []
-        lineage = []
-        for lineage_entry in lineage_info:
-            table_id = lineage_entry['table']
-            logging.debug('table_id = {}, table_id_to_bqtable.keys={}'.format(table_id, str(table_id_to_bqtable)))
-            bq_table = table_id_to_bqtable.get(table_id)
-            stats_values = {}
-            if bq_table is not None:
-                ds, sc = BqCommonHelpers.table_to_kensu(bq_table)
-                ds_path = ds.pk.location
-                sc = [(f.name, f.field_type) for f in sc.pk.fields]
-                table_stats_info = stats_info.get(table_id, {})
-                stats_aggs = table_stats_info.get('stats')
-                stats_filters = table_stats_info.get('input_filters')
-                bg_table_ref = bq_table.reference
-                # note: making stats computation lazy in a f_get_stats lambda seem to behave very weirdly...
-                # so stats are computed eagerly now
-                stats_values = compute_bigquery_stats(
-                    table_ref=bg_table_ref,
-                    table=bq_table,
-                    client=client,
-                    stats_aggs=stats_aggs,
-                    input_filters=stats_filters)
-            else:
-                sc = None
-                ds_path = 'bigquery:/' + table_id  # FIXME: add proper BQ prefix, and extract a shared helper
+        lineage = list([BqRemoteParser.convert_lineage_entry(
+                lineage_entry,
+                kensu=kensu,
+                client=client,
+                table_id_to_bqtable=table_id_to_bqtable,
+                stats_info=stats_info
+            ) for lineage_entry in lineage_info])
+        converted_lineage = GenericComputedInMemDs(lineage=lineage)
+        logging.debug('converted_lineage:' + str(converted_lineage))
+        return converted_lineage
 
-            logging.debug(f'table_id {table_id} (table.ref={bg_table_ref}, ds_path: {ds_path}) got input_filters: {stats_filters} & stat_aggs:{str(stats_aggs)}')
-            input = KensuDatasourceAndSchema.for_path_with_opt_schema(
-                kensu,
-                ds_path=ds_path,
-                format='BigQuery table',
-                categories=None,
-                maybe_schema=sc,
-                f_get_stats=lambda: stats_values
-            )
-            lin_entry = ExtDependencyEntry(
-                input_ds=input,
-                lineage=lineage_entry['mapping']
-            )
-            inputs.append(input)
-            lineage.append(lin_entry)
-        lineage_info = GenericComputedInMemDs(inputs=inputs, lineage=lineage)
-        logging.debug('final lineage' + str(lineage_info))
-        return lineage_info
+    @staticmethod
+    def convert_lineage_entry(lineage_entry, kensu, client: bq.Client, table_id_to_bqtable, stats_info):
+        table_id = lineage_entry['table']
+        logging.debug('table_id = {}, table_id_to_bqtable.keys={}'.format(table_id, str(table_id_to_bqtable)))
+        bq_table = table_id_to_bqtable.get(table_id)
+        stats_values = {}
+        if bq_table is not None:
+            ds, sc = BqCommonHelpers.table_to_kensu(bq_table)
+            ds_path = ds.pk.location
+            sc = [(f.name, f.field_type) for f in sc.pk.fields]
+            table_stats_info = stats_info.get(table_id, {})
+            stats_aggs = table_stats_info.get('stats')
+            stats_filters = table_stats_info.get('input_filters')
+            bg_table_ref = bq_table.reference
+            # note: making stats computation lazy in a f_get_stats lambda seem to behave very weirdly...
+            # so stats are computed eagerly now
+            stats_values = compute_bigquery_stats(
+                table_ref=bg_table_ref,
+                table=bq_table,
+                client=client,
+                stats_aggs=stats_aggs,
+                input_filters=stats_filters)
+            logging.debug(
+                f'table_id {table_id} (table.ref={bg_table_ref}, ds_path: {ds_path}) got input_filters: {stats_filters} & stat_aggs:{str(stats_aggs)}')
+        else:
+            sc = None
+            ds_path = 'bigquery:/' + table_id  # FIXME: add proper BQ prefix, and extract a shared helper
+        input = KensuDatasourceAndSchema.for_path_with_opt_schema(
+            kensu,
+            ds_path=ds_path,
+            format='BigQuery table',
+            categories=None,
+            maybe_schema=sc,
+            f_get_stats=lambda: stats_values
+        )
+        return ExtDependencyEntry(
+            input_ds=input,
+            lineage=lineage_entry['mapping']
+        )
