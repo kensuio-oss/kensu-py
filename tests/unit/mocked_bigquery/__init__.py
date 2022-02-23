cf034e31ff4b6f80b3d969751d9e62551a376a8f
vidmantas zemleris <vidma@kensu.io>
2022-02-23 18:47:27 +0200

    upd bigquery spec

--- tests/unit/mocked_bigquery/__init__.py
+++ tests/unit/mocked_bigquery/__init__.py
@@ -10,8 +10,8 @@ from kensu.google.cloud import bigquery as kensu_bigquery
 from kensu.google.cloud.bigquery.job.offline_parser import BqOfflineParser
 
 sample_sql = f"""SELECT DATE_KEY, "ARG" AS COUNTRY_KEY, s.STORE_KEY, CHAIN_TYPE_DESC, CARD_FLAG, sum(TOTAL) AS CA 
-            FROM `psyched-freedom-306508.cf.ARG-stores` AS s 
-            INNER JOIN `psyched-freedom-306508.cf.ARG-tickets` AS t 
+            FROM `psyched-freedom-306508`.`cf`.ARG-stores` AS s
+            INNER JOIN `psyched-freedom-306508.cf.ARG-tickets` AS t
             ON s.STORE_KEY = t.STORE_KEY
             WHERE DATE_KEY = "2021-05-23" 
             GROUP BY DATE_KEY, s.STORE_KEY, CHAIN_TYPE_DESC, CARD_FLAG"""
