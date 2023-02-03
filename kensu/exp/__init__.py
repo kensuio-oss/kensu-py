791206fa64448d4a05129280dd1dba1c42778287
Emanuele Lucchini <emanuele.lucchini@kensu.io>
2023-02-03 16:29:15 +0100

    fix guid for process and process run if spark not available

--- kensu/exp/__init__.py
+++ kensu/exp/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 import urllib3
+
 urllib3.disable_warnings()
 from kensu.utils.kensu import Kensu
 from kensu.client.models import *
@@ -14,11 +15,17 @@ from kensu.utils.kensu_provider import KensuProvider
 
 
 def get_spark_session():
-    import gc
-    for obj in gc.get_objects():
-        if isinstance(obj, SparkSession):
-            kensu_spark_session = obj
-    return kensu_spark_session or None
+    kensu_spark_session = None
+    try:
+        import gc
+        for obj in gc.get_objects():
+            if isinstance(obj, SparkSession):
+                kensu_spark_session = obj
+    except:
+        return None
+    else:
+        return kensu_spark_session
+
 
 import types
 
@@ -44,6 +51,7 @@ def register_in_mem_schema(
     k.name_schema_lineage_dict[in_mem_name] = schema.to_guid()
     return schema
 
+
 """
 Tag a Spark DataFrame as in-mem datasource, assuming that it will be transformed into python by using say .toPandas().
 The result in Kensu will be: [spark input{1,2,..}] --lineage--> [in-mem: name]
@@ -54,6 +62,8 @@ how spark works - DataFrame is never materialized before "write"), i.e.:
     `spark_df.tagInMem('name1').filter('col1 = 1').tagInMem('name2')`
 would create two UNRELATED in-mem data-sources.
 """
+
+
 def tagInMem(self,  # type: DataFrame
              name):
     try:
@@ -68,7 +78,7 @@ def tagInMem(self,  # type: DataFrame
         process = spark_info['process_guid']
 
         # Creation of an in-mem data source
-        spark_ds_name=f'{process}/{name}'
+        spark_ds_name = f'{process}/{name}'
         # location = f'in-memory-data://{process}/{name}'
         location = f'in-memory-data://{spark_ds_name}'
         format = 'in-mem'
@@ -111,29 +121,30 @@ def report_spark_lineage(spark_lineage,
                          k,  # type: Kensu
                          result_schema,
                          output_name):
-        # similar like GenericComputedInMemDs.report, but using processRun from spark and no Kensu-Py magic
-        from kensu.utils.helpers import extract_ksu_ds_schema
-        spark_input_names = []
-        for input_ds in spark_lineage.inputs:
-            ds, schema = extract_ksu_ds_schema(k, input_ds, report=True, register_orig_data=False)
-            input_name = ds.pk.location
-            spark_input_names.append(input_name)
-            k.name_schema_lineage_dict[input_name] = schema.to_guid()
+    # similar like GenericComputedInMemDs.report, but using processRun from spark and no Kensu-Py magic
+    from kensu.utils.helpers import extract_ksu_ds_schema
+    spark_input_names = []
+    for input_ds in spark_lineage.inputs:
+        ds, schema = extract_ksu_ds_schema(k, input_ds, report=True, register_orig_data=False)
+        input_name = ds.pk.location
+        spark_input_names.append(input_name)
+        k.name_schema_lineage_dict[input_name] = schema.to_guid()
 
-        # report output (if needed)
-        lineage_run = link(input_names=spark_input_names, output_name=output_name)
-        lineage_run_id = lineage_run.to_guid()
-        # ask spark to publish stats given a lineage-run
-        # FIXME: do we need to activate/deactivate it depending on config? but kensupy conf or pyspark conf?
-        for input_ds in spark_lineage.inputs:
-            input_ds.f_publish_stats(lineage_run_id)
+    # report output (if needed)
+    lineage_run = link(input_names=spark_input_names, output_name=output_name)
+    lineage_run_id = lineage_run.to_guid()
+    # ask spark to publish stats given a lineage-run
+    # FIXME: do we need to activate/deactivate it depending on config? but kensupy conf or pyspark conf?
+    for input_ds in spark_lineage.inputs:
+        input_ds.f_publish_stats(lineage_run_id)
 
 
 def tagInMemWrapper():
     def tagInMemInner(self,  # type: DataFrame
-                      name   # type: str
+                      name  # type: str
                       ):
         return tagInMem(self, name)
+
     return tagInMemInner
 
 
@@ -141,9 +152,9 @@ def tagCreateSparkDataFrameFromPy(
         self,  # type: DataFrame
         name,  # type: str
         input_names=None  # type: list[str]
-       ):
+):
     try:
-        #Get spark session info
+        # Get spark session info
         spark = get_spark_from_df(self)
         spark_info = get_process_run_info(spark)
         process = spark_info['process_guid']
@@ -158,7 +169,8 @@ def tagCreateSparkDataFrameFromPy(
 
         # FIXME: only spark dataframe supported now, i.e. this function must be after spark.createDataFrame()
         fields = get_schema_fields_from_spark_df(spark_df=self)
-        schema = Schema(location, pk=SchemaPK(data_source_ref=DataSourceRef(by_guid=ds.to_guid()), fields=fields))._report()
+        schema = Schema(location,
+                        pk=SchemaPK(data_source_ref=DataSourceRef(by_guid=ds.to_guid()), fields=fields))._report()
         k.name_schema_lineage_dict[name] = schema.to_guid()
 
         # report lineage to Kensu between Python inputs and spark.createDataFrame which is not seen by Spark directly
@@ -195,10 +207,11 @@ def tagCreateSparkDataFrameFromPy(
 
 def tagCreateDataFrameWrapper():
     def tagInMemInner(self,  # type: DataFrame
-                      name,   # type: str
+                      name,  # type: str
                       input_names=None
                       ):
         return tagCreateSparkDataFrameFromPy(self, name=name, input_names=input_names)
+
     return tagInMemInner
 
 
@@ -212,69 +225,82 @@ def create_publish_for_data_source(name, format, location, schema=None, **kwargs
     ds_pk = DataSourcePK(location=location,
                          physical_location_ref=PhysicalLocationRef(by_pk=k.UNKNOWN_PHYSICAL_LOCATION.pk))
 
-    ds = DataSource(name=name,format=format, categories=[f'logical::{name}'],pk=ds_pk)._report()
+    ds = DataSource(name=name, format=format, categories=[f'logical::{name}'], pk=ds_pk)._report()
 
     if schema is not None:
         fields = schema
     else:
-        fields = [FieldDef('unknown','unknown',True)]
-    schema = Schema(name, pk=SchemaPK(data_source_ref=DataSourceRef(by_guid=ds.to_guid()),fields=fields))._report()
+        fields = [FieldDef('unknown', 'unknown', True)]
+    schema = Schema(name, pk=SchemaPK(data_source_ref=DataSourceRef(by_guid=ds.to_guid()), fields=fields))._report()
     k.name_schema_lineage_dict[name] = schema.to_guid()
 
+
 def create_publish_for_postgres_table(table, location, cur=None):
-    format='Postgres Table'
+    format = 'Postgres Table'
     from kensu.psycopg2.pghelpers import get_table_schema
     try:
         if cur:
-            SchemaFields = get_table_schema(cur=cur,table_name=table)
+            SchemaFields = get_table_schema(cur=cur, table_name=table)
             from kensu.client.models import FieldDef
-            schema_ = [FieldDef(f['field_name'],f['field_type'],True) for f in SchemaFields]
+            schema_ = [FieldDef(f['field_name'], f['field_type'], True) for f in SchemaFields]
         else:
             schema_ = None
     except:
-        schema_=None
+        schema_ = None
     create_publish_for_data_source(ds=table, name=location, location=location, format=format, schema=schema_)
 
+
 def create_publish_for_csv(name, location):
-    format='csv'
+    format = 'csv'
     import pandas as pd
     from kensu.pandas.extractor import KensuPandasSupport
-    schema_=None
+    schema_ = None
     create_publish_for_data_source(ds=name, name=name, location=location, format=format, schema=schema_)
+
 
 def create_publish_for_sklearn_model(name, location):
     format = 'SKLearn'
     k = KensuProvider().instance()
     ds_pk = DataSourcePK(location=location,
                          physical_location_ref=PhysicalLocationRef(by_pk=k.UNKNOWN_PHYSICAL_LOCATION.pk))
-    ds = DataSource(name=name,format=format, categories=[f'logical::{name}'],pk=ds_pk)._report()
+    ds = DataSource(name=name, format=format, categories=[f'logical::{name}'], pk=ds_pk)._report()
 
-    fields = [FieldDef('intercept','Numeric',False),FieldDef('coeff','Numeric',False)]
-    schema = Schema(name, pk=SchemaPK(data_source_ref=DataSourceRef(by_guid=ds.to_guid()),fields=fields))._report()
+    fields = [FieldDef('intercept', 'Numeric', False), FieldDef('coeff', 'Numeric', False)]
+    schema = Schema(name, pk=SchemaPK(data_source_ref=DataSourceRef(by_guid=ds.to_guid()), fields=fields))._report()
     k.name_schema_lineage_dict[name] = schema.to_guid()
 
 
 def get_schema(name):
-   # use the lookup table
+    # use the lookup table
 
-   k = KensuProvider().instance()
-   if name in k.name_schema_lineage_dict:
-       return k.name_schema_lineage_dict[name]
-   else:
-       logging.info(f'Unable to retrieve Kensu GUID for schema {name}')
-       return None
+    k = KensuProvider().instance()
+    if name in k.name_schema_lineage_dict:
+        return k.name_schema_lineage_dict[name]
+    else:
+        logging.info(f'Unable to retrieve Kensu GUID for schema {name}')
+        return None
 
 
 def create_lineage(inputs, output):
     data_flow = [SchemaLineageDependencyDef(from_schema_ref=SchemaRef(by_guid=i),
                                             to_schema_ref=SchemaRef(by_guid=output),
                                             # FIXME: or should we report column_data_dependencies=None ?
-                                            column_data_dependencies={'unknown': ['unknown']}) for i in inputs ]
+                                            column_data_dependencies={'unknown': ['unknown']}) for i in inputs]
+
+    # Definition of the process
+    if get_spark_session() != None:
+        process_guid = get_process_run_info(get_spark_session())['process_guid']
+    else:
+        k = KensuProvider().instance()
+        process_guid = k.process.to_guid()
+
     lineage = ProcessLineage(name=f'Lineage to {str(output)}',  # FIXME: List inputs too?
                              operation_logic='APPEND',
-                             pk=ProcessLineagePK(process_ref=ProcessRef(by_guid=get_process_run_info(get_spark_session())['process_guid']),
+                             pk=ProcessLineagePK(process_ref=ProcessRef(
+                                 by_guid=process_guid),
                                                  data_flow=data_flow))._report()
     return lineage
+
 
 def link(input_names, output_name):
     # retrieve schemas for inputs
@@ -282,14 +308,14 @@ def link(input_names, output_name):
     # retrieve schemas for outputs
     output_sc = get_schema(output_name)
     # create lineage
-    lineage = create_lineage(input_scs,output_sc)._report()
+    lineage = create_lineage(input_scs, output_sc)._report()
 
-    #Definition of the process run
-    process_run_guid = get_process_run_info(get_spark_session())['process_run_guid']
-
-    k = KensuProvider().instance()
-    process_run_guid = k.process_run.to_guid()
-
+    # Definition of the process run
+    if get_spark_session() != None:
+        process_run_guid = get_process_run_info(get_spark_session())['process_run_guid']
+    else:
+        k = KensuProvider().instance()
+        process_run_guid = k.process_run.to_guid()
 
     # create lineage run
     k = KensuProvider().instance()
