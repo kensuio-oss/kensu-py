20ef8cabf86de14ce8d7fc0f2c18061be1cef3aa
vidmantas zemleris <vidma@kensu.io>
2023-10-05 16:21:02 +0300

    impl tables

--- kensu/pyspark/KensuDataFrameWriter.py
+++ kensu/pyspark/KensuDataFrameWriter.py
@@ -112,10 +112,11 @@ class KensuDataFrameWriter:
         self._delay_fn_call(lambda: self._df_writer.sortBy(col, *cols))
         return self
 
-    def _handle_simple_format_save(self,
-                                   path: Optional[str] = None,
-                                   format: Optional[str] = None
-                                   ):
+    def _handle_simple_save(self,
+                            path: Optional[str] = None,
+                            format: Optional[str] = None,
+                            table_name: Optional[str] = None
+                            ):
         if path is None:
             path = self._options.get("path", None)
         if path is not None:
@@ -124,18 +125,19 @@ class KensuDataFrameWriter:
                 from kensu.pyspark.spark_connector import addOutputObservationsWithRemoteConf
                 kensu_efficient_write_compute_count_distinct = False  # FIXME: configure in a different way, if needed
                 import logging
-                logging.info("KENSU: DataFrameWriter for output path={} format={}, will be automatically updated with Kensu observations via .observe() using remote config if enabled".format(path, format))
+                logging.info("KENSU: DataFrameWriter for output path={} table_name={} format={}, will be automatically updated with Kensu observations via .observe() using remote config if enabled".format(path, table_name, format))
                 df = addOutputObservationsWithRemoteConf(df,
                                                          path=path,
-                                                         table_name=None,
+                                                         table_name=table_name,
                                                          format=format,
                                                          compute_count_distinct=kensu_efficient_write_compute_count_distinct)
-                logging.info("KENSU: DataFrameWriter for output path={} format={}, was updated with Kensu observations via .observe() using remote config if enabled".format(path, format))
+                logging.info("KENSU: DataFrameWriter for output path={}  table_name={} format={}, was updated with Kensu observations via .observe() using remote config if enabled".format(path, table_name, format))
             except:
                 import traceback
                 import logging
                 logging.info(
-                    "KENSU: unexpected issue when adding output observations, are you using old kensu Jar?: {}".format(
+                    "KENSU: unexpected issue when adding output observations to output path={}  table_name={} format={}, are you using old kensu Jar?: {}".format(
+                        path, table_name, format,
                         traceback.format_exc()))
             self._update_df(df)
         self._call_deferred_fns()
@@ -148,11 +150,11 @@ class KensuDataFrameWriter:
         partitionBy: Optional[Union[str, List[str]]] = None,
         **options: "OptionalPrimitiveType",
     ) -> None:
-        self._handle_simple_format_save(path, format or self._options.get('format'))
+        self._handle_simple_save(path, format or self._options.get('format'))
         return self._df_writer.save(path=path, format=format, mode=mode, partitionBy=partitionBy, **options)
 
     def insertInto(self, tableName: str, overwrite: Optional[bool] = None) -> None:
-        # fixme: impl observe
+        self._handle_simple_save(path=None, table_name=tableName)
         return self._df_writer.insertInto(tableName, overwrite)
 
     def saveAsTable(
@@ -163,27 +165,27 @@ class KensuDataFrameWriter:
         partitionBy: Optional[Union[str, List[str]]] = None,
         **options: "OptionalPrimitiveType",
     ) -> None:
-        # fixme: impl observe
+        self._handle_simple_save(path=None, table_name=name, format=format or self._options.get('format'))
         return self._df_writer.saveAsTable(name=name, format=format, mode=mode, partitionBy=partitionBy, **options)
 
     def json(self, *args, **kwargs) -> None:
-        self._handle_simple_format_save(path=self._path_from_args(args, kwargs), format="json")
+        self._handle_simple_save(path=self._path_from_args(args, kwargs), format="json")
         return self._df_writer.json(*args, **kwargs)
 
     def parquet(self, *args, **kwargs) -> None:
-        self._handle_simple_format_save(path=self._path_from_args(args, kwargs), format="parquet")
+        self._handle_simple_save(path=self._path_from_args(args, kwargs), format="parquet")
         return self._df_writer.parquet(*args, **kwargs)
 
     def text(self, *args, **kwargs) -> None:
-        self._handle_simple_format_save(path=self._path_from_args(args, kwargs), format="text")
+        self._handle_simple_save(path=self._path_from_args(args, kwargs), format="text")
         return self._df_writer.text(*args, **kwargs)
 
     def csv(self, *args, **kwargs) -> None:
-        self._handle_simple_format_save(path=self._path_from_args(args, kwargs), format="csv")
+        self._handle_simple_save(path=self._path_from_args(args, kwargs), format="csv")
         return self._df_writer.csv(*args, **kwargs)
 
     def orc(self, *args, **kwargs) -> None:
-        self._handle_simple_format_save(path=self._path_from_args(args, kwargs), format="orc")
+        self._handle_simple_save(path=self._path_from_args(args, kwargs), format="orc")
         return self._df_writer.orc(*args, **kwargs)
 
     def jdbc(
