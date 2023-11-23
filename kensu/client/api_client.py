6ff58bbb4ee1fccd17df9561c2efb272574f7f3b
Andy Petrella <andy.petrella@kensu.io>
2023-11-23 11:34:12 +0100

    PID-540

--- kensu/client/api_client.py
+++ kensu/client/api_client.py
@@ -72,7 +72,7 @@ class ApiClient(object):
             self.host = host
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/0.0.3/python'
+        self.user_agent = 'Swagger-Codegen-Python'
 
     @property
     def user_agent(self):
